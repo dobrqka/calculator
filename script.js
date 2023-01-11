2fa02a9ea802c8223a111a5e6e0af78bb970a157
dobrqka <dobrin.andreev@gmail.com>
2023-01-11 15:06:13 +0200

    Tidy up js code and fix weird decimal results
    Wrapped repeating code in functions, removed unused code,
    fixed an issue with weird decimal values in some results.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// this will perform an operation with 2 numbers
+
 function add(a, b) {
     return (+a)+(+b);
 }
@@ -6,241 +8,223 @@ function subtract(a, b) {
     return a-b;
 }
 
- function multiply(a, b) {
-     return a*b;
- }
+function multiply(a, b) {
+    return a*b;
+}
 
- function divide(a, b) {
+function divide(a, b) {
     return a/b;
- }
+}
+
+ // this will perform a calculation after getting the operator and 2 numbers
 
 function operate(operator, a, b) {
     if (operator == '+') {
         return add(a, b);
     }
     else if (operator == '-') {
-        return subtract (a, b);
+        return subtract(a, b);
     }
     else if (operator == '*') {
         return multiply(a, b);
     }
     else if (operator == '/') {
-            return divide(a, b);
-        }
-}
-
-const display = document.querySelector('.display')
-let displayValue;
-let operation;
-
-function populateDisplay() {
-    const numbers = document.querySelectorAll('.number');
-    for (i=0; i<numbers.length; i++) {
-        numbers[i].addEventListener('click', (e) => {
-            for (i=0; i<operators.length; i++) {
-                operators[i].classList.remove("toggle-button");
-              }
-            if (display.textContent == 0 || display.textContent == displayValue) {
-                display.textContent = e.target.textContent;
-            }
-            else {
-                display.textContent += e.target.textContent;
-            }
-        });
+        return divide(a, b);
     }
 }
 
+const display = document.querySelector('.display');
+let displayValue;
+let operation;
+
+// populates display with number from input and removes the toggle of the previously selected operator button
+
+const numbers = document.querySelectorAll('.number');
+
+for (i=0; i<numbers.length; i++) {
+    numbers[i].addEventListener('click', (e) => {
+        for (i=0; i<operators.length; i++) {
+            operators[i].classList.remove("toggle-button");
+        }
+        if (display.textContent == 0 || display.textContent == displayValue) {
+            display.textContent = e.target.textContent;
+        }
+        else {
+            display.textContent += e.target.textContent;
+        }
+    });
+}
+
+const operators = document.querySelectorAll('.operation');
 
 // keyboard support for numbers
 
 document.addEventListener('keydown', (e) => {
-   if (isNaN(e.key) == false) {
-    for (i=0; i<operators.length; i++) {
-        operators[i].classList.remove("toggle-button");
-      }
-      if (display.textContent == 0 || display.textContent == displayValue) {
-        display.textContent = e.key
-      }
-      else {
-        display.textContent += e.key
-      }
-   }
+    if (isNaN(e.key) == false) {
+        for (i=0; i<operators.length; i++) {
+            operators[i].classList.remove("toggle-button");
+        }
+        if (display.textContent == 0 || display.textContent == displayValue) {
+            display.textContent = e.key;
+        }
+        else {
+            display.textContent += e.key;
+        }
+    }
 });
 
-populateDisplay();
+// function that saves the current result in a variable based on the selected operator
 
-const operators = document.querySelectorAll('.operation');
-
-function currentOperation() {
-    for (i=0; i<operators.length; i++) {
-        // operators[i].addEventListener('click', () => {displayValue = operate(operation, displayValue, display.textContent)})
-        // operators[i].addEventListener('click', () => {displayValue = display.textContent});
-
-        operators[i].addEventListener('click', () => {
-            if (displayValue == undefined) {
-                displayValue = display.textContent;
-            }
-            else {
-                displayValue = operate(operation, displayValue, display.textContent);
-            }
-        })
-        operators[i].addEventListener('click', () => {console.log(displayValue)});
-        operators[i].addEventListener('click', (e) => {operation = e.target.textContent});
-        operators[i].addEventListener('click', (e) => {e.target.classList.add("toggle-button")});
-        operators[i].addEventListener('click', () => {display.textContent = displayValue});
+function currentResult() {
+    if (displayValue == undefined) {
+        displayValue = display.textContent;
     }
+    else {
+        displayValue = operate(operation, displayValue, display.textContent);
+    }
+    display.textContent = displayValue;
 }
+
+// saves the selected operator and displays the current result
+
+for (i=0; i<operators.length; i++) {
+    operators[i].addEventListener('click', currentResult);
+    operators[i].addEventListener('click', (e) => {
+        operation = e.target.textContent;
+        e.target.classList.add("toggle-button");
+    });
+}
+
+// keyboard support for operations
 
 document.addEventListener('keydown', (e) => {
     if (e.key == '+') {
-        if (displayValue == undefined) {
-            displayValue = display.textContent;
-        }
-        else {
-            displayValue = operate(operation, displayValue, display.textContent);
-        }
+        currentResult();
         operation = '+';
         document.querySelector("#add").classList.add("toggle-button");
-        display.textContent = displayValue;
     }
     else if (e.key == '-') {
-        if (displayValue == undefined) {
-            displayValue = display.textContent;
-        }
-        else {
-            displayValue = operate(operation, displayValue, display.textContent);
-        }
+        currentResult();
         operation = '-';
         document.querySelector("#subtract").classList.add("toggle-button");
-        display.textContent = displayValue;
     }
     else if (e.key == '*') {
-        if (displayValue == undefined) {
-            displayValue = display.textContent;
-        }
-        else {
-            displayValue = operate(operation, displayValue, display.textContent);
-        }
+        currentResult();
         operation = '*';
         document.querySelector("#multiply").classList.add("toggle-button");
-        display.textContent = displayValue;
     }
     else if (e.key == '/') {
-        if (displayValue == undefined) {
-            displayValue = display.textContent;
-        }
-        else {
-            displayValue = operate(operation, displayValue, display.textContent);
-        }
+        currentResult();
         operation = '/';
         document.querySelector("#divide").classList.add("toggle-button");
-        display.textContent = displayValue;
     }
 })
 
 let secondValue;
-
-currentOperation();
-
 const equalsButton = document.querySelector('.equals');
 
-equalsButton.addEventListener('click', () => {secondValue = display.textContent});
-equalsButton.addEventListener('click', () => {
+// shows the final answer, rounds up digits after decimal to 6 digits to prevent overflowing the screen
+// and prevents division by 0 notifying the user that they cannot do it
+
+function finalCalculation() {
+    secondValue = display.textContent;
     if (operation == '/' && secondValue == 0) {
         alert('Division by 0? Are you trying to break the universe?');
         secondValue = undefined;
         display.textContent = displayValue;
         document.querySelector("#divide").classList.add("toggle-button");
     }
-    else { 
-        display.textContent = operate(operation, displayValue, secondValue)
+    else {
+        display.textContent = operate(operation, displayValue, secondValue);
         if (Number(display.textContent) % 1 != 0 && Number(display.textContent).toString().split('.')[1].length > 6) {
-            display.textContent = Number(display.textContent).toFixed(6);
+            display.textContent = Math.floor((Number(display.textContent))*1000000)/1000000;
         }
     }
-});
+}
+
+equalsButton.addEventListener('click', finalCalculation);
+
+// keyboard support for the equals button
+
 document.addEventListener('keydown', (e) => {
     if (e.key == '=' || e.key == 'Enter') {
         e.preventDefault();
-        secondValue = display.textContent;
-        if (operation == '/' && secondValue == 0) {
-            alert('Division by 0? Are you trying to break the universe?');
-            secondValue = undefined;
-            display.textContent = displayValue;
-            document.querySelector("#divide").classList.add("toggle-button");
-        }
-        else {
-            display.textContent = operate(operation, displayValue, secondValue);
-            if (Number(display.textContent) % 1 != 0 && Number(display.textContent).toString().split('.')[1].length > 6) {
-            display.textContent = Number(display.textContent).toFixed(6);
-            }
-        }
+        finalCalculation();
     }
 });
-// button to reset
+
+// button to reset everything
 
 const clearButton = document.querySelector('.clear');
-clearButton.addEventListener('click', () => {operation = undefined});
-clearButton.addEventListener('click', () => {displayValue = undefined});
-clearButton.addEventListener('click', () => {secondValue = undefined});
-clearButton.addEventListener('click', () => {display.textContent = '0'});
-clearButton.addEventListener('click', () => {
+
+function clearFunction() {
+    operation = undefined;
+    displayValue = undefined;
+    secondValue = undefined;
+    display.textContent = 0;
     for (i=0; i<operators.length; i++) {
         operators[i].classList.remove("toggle-button");
-      }
-})
+    }
+}
+
+clearButton.addEventListener('click', clearFunction);
+
+// keyboard support for reset button
+
 document.addEventListener('keydown', (e) => {
     if (e.key == 'Escape') {
-        operation = undefined;
-        displayValue = undefined;
-        secondValue = undefined;
-        display.textContent = '0';
-        for (i=0; i<operators.length; i++) {
-            operators[i].classList.remove("toggle-button");
-          }
+       clearFunction();
     }
 });
 
 // clear latest digit
 
 const backspace = document.querySelector('.backspace');
-backspace.addEventListener('click', () => {display.textContent = display.textContent.substring(0,display.textContent.length-1)})
+
+function doBackspace() {
+    display.textContent = display.textContent.substring(0,display.textContent.length-1);
+}
+
+backspace.addEventListener('click', doBackspace);
+
+// keyboard support for backspace
+
 document.addEventListener('keydown', (e) => {
-    if (e.key == 'Backspace')
-    {
-        display.textContent = display.textContent.substring(0,display.textContent.length-1)
+    if (e.key == 'Backspace') {
+        doBackspace();
     }
-})
+});
 
-// document.addEventListener('keydown', (e) => {console.log(e.key)})
-
-
-// add a . button for decimals, don't let the user type more than one decimal symbol
+// button for decimals, prevent the user from using more than one decimal symbol
 
 const decimal = document.querySelector('.decimal');
-decimal.addEventListener('click', () => {
+
+function addDecimal() {
     if (display.textContent.includes('.')) {
         return;
     }
     else {
-        display.textContent += '.'
+        display.textContent += '.';
     }
-});
+}
+
+decimal.addEventListener('click', addDecimal);
+
+// keyboard support for decimal sign
+
 document.addEventListener('keydown', (e) => {
     if (e.key == '.') {
-        if (display.textContent.includes('.')) {
-            return;
-        }
-        else {
-            display.textContent += '.'
-        }
+        addDecimal();
     }
-})
+});
+
+// button that converts the number to a negative number
 
 const negative = document.querySelector('.negative');
+
 negative.addEventListener('click', () => {
     display.textContent = -display.textContent;
-})
+});
 
 document.addEventListener('keydown', (e) => {
     if (e.key == '_') {
@@ -249,4 +233,4 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
-// tidy up js code
+// final css stuff
